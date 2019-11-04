221fed4c791d2b4676ed9c8263babe0459664156
nyamel <twilight6sachirin@gmail.com>
2019-11-04 22:05:32 +0900

    だいたい完成 alpha channel 有効なpngは別で解凍。

--- test.py
+++ test.py
@@ -2,27 +2,51 @@
 from pathlib import Path
 import zipfile
 import pprint
+import img2pdf
+from wand.image import Image
 
-ZIP_FILE = './test-20191103T160942Z-001.zip'
+ZIP_FILE = r'./test-20191103T160942Z-001.zip'
 
 zipfilepointer = zipfile.ZipFile(ZIP_FILE)
 
-# print(len(zipfilepointer.namelist())) 項目数獲得成功！
+# print(len(zipfilepointer.namelist())) 項目数獲得成功
 
 zip_list = zipfilepointer.namelist()
-
 # pprint.pprint(file_list)
 
 path_list = [Path(f'./{i}') for i in zip_list]
 
 ex_li = ['.jpg', '.jpeg', '.jpe', '.jfif', '.png', '.gif',
-               '.pdf', '.tif', '.tiff', '.nsk', '.bmp', '.dib', '.rle']
+                '.tif', '.tiff', '.nsk', '.bmp', '.dib', '.rle']
 EX_li = list(map(lambda x: x.upper(), ex_li))
 # print(EX_li)
 
 Extends = set(ex_li + EX_li)
 
-image_list = [p for p in path_list if p.suffix in Extends]
+image_list = [str(p) for p in path_list if p.suffix in Extends]
 
+Sucsess_list =[]
+Error_list = []
+
+
+i = 0
 for image in image_list:
-    zipfilepointer.extract(str(image), './test1')
+    try:
+        with zipfilepointer.open(image) as img:
+            with open(f'test{i}.pdf','wb') as op:
+                op.write(img2pdf.convert(img))
+                op.close()
+    except:
+        print('Error:',image)
+        zipfilepointer.extract(image)
+        Error_list.append(image)
+    else:
+        print('Sucsess:',image)
+        Sucsess_list.append(image)
+        i += 1
+
+
+pprint.pprint(Sucsess_list)
+pprint.pprint(Error_list)
+
+# alpha channelの警告文: img2pdf ソースコード 1038行目付近
