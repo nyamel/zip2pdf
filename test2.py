3146abf6dc3d1c71120a84e4f42f4a1de8716aa0
nyamel <twilight6sachirin@gmail.com>
2019-11-07 03:45:39 +0900

    Replace faild file and remove empty directry.

---
+++ test2.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+import zipfile
+import shutil
+from pprint import pprint
+import img2pdf
+
+folder = Path('test-20191103T160942Z-001/Faild')
+
+pprint(list(folder.glob('**/*')))
+
+i = 0
+for p in folder.glob('**/*'):
+    if p.is_file():
+        p.replace(folder/p.name)
+        i += 1
+
+
+for p in folder.glob('*'):
+    if p.is_dir():
+        shutil.rmtree(p)
